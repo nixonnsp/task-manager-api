c2a7e81f863a03f9d29263e14da7596915048814
Nixon da Silva Palmeira <nixonnsp@gmail.com>
2020-03-29 21:17:03 +0000

    Adiconando versões de api nas rotas

---
+++ lib/api_version_constraint.rb
@@ -0,0 +1,12 @@
+class ApiVersionConstraint
+  def initialize(options)
+    @version = options[:version]    
+    @default = options[:default]
+  end
+
+  def matches?(req)
+    @default || req.readers['Accept'].include?("application/vnd.taskmanager.v#{@version}")
+  end
+  
+  
+end
