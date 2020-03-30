74b5dc6a631ffbfe5d12649bb7f06731c0651c11
Nixon da Silva Palmeira <nixonnsp@gmail.com>
2020-03-30 22:24:16 +0000

    ajustando rspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryGirl.build :user
+  end 
+
+  it { expect(@user).to respond_to(:email) }
+  it { expect(@user).to respond_to(:name) }
+  it { expect(@user).to respond_to(:password) }
+  it { expect(@user).to respond_to(:password_confirmation) }
+  it { expect(@user).to be_valid }
+
 end
