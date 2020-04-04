378f419366023d0adf899550bec2b636669273fd
Nixon da Silva Palmeira <nixonnsp@gmail.com>
2020-04-04 21:13:19 +0000

    Alterando aspas duplas para simples

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   #subject { build(:user) } 
-  #pending "add some examples to (or delete) #{__FILE__}"
+  #pending 'add some examples to (or delete) #{__FILE__}'
   #before do
   #  @user = FactoryGirl.build :user
   #end 
