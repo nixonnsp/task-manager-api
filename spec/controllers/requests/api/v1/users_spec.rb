345bb53f17c8deab6911111a140a3d31cfe256bb
Nixon da Silva Palmeira <nixonnsp@gmail.com>
2020-04-05 15:34:26 +0000

    Implementado a actiou update do controler users

--- spec/controllers/requests/api/v1/users_spec.rb
+++ spec/controllers/requests/api/v1/users_spec.rb
@@ -14,8 +14,8 @@ RSpec.describe 'Users API', type: :request do
     
     context 'when the user exists' do
       it 'returns the user' do       
-        user_response = JSON.parse(response.body)
-        expect(user_response['id']).to eq(user_id)  
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:id]).to eq(user_id)  
       end
 
       it 'returns status code 200' do
@@ -45,8 +45,8 @@ RSpec.describe 'Users API', type: :request do
       end
 
       it 'returns json data for the created user' do
-        user_response = JSON.parse(response.body)
-        expect(user_response['email']).to eq(user_params[:email])
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:email]).to eq(user_params[:email])
       end
     end
 
@@ -58,10 +58,47 @@ RSpec.describe 'Users API', type: :request do
       end
 
       it 'returns the json data for the erros' do
-        user_response = JSON.parse(response.body)
-        expect(user_response).to have_key('errors')
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response).to have_key(:errors)
       end
     end
   end
   
+  describe 'PUT /users/:id' do
+    before do
+      headers = { 'Accept' => 'application/vnd.taskmanager.v1'}
+      put "/users/#{user_id}", params: { user: user_params}, headers: headers
+    end
+
+    context 'when the request params are valid' do
+      let(:user_params) { { email: 'new_email@task.com'} }
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+
+      it 'returns the json data for the updated user' do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:email]).to eq(user_params[:email]) 
+      end
+
+    end
+
+    context 'when the request params are invalid' do
+      let(:user_params) { { email: 'new_email@'} }
+
+      it 'returns status code 422' do
+        expect(response).to have_http_status(422)
+      end
+
+      it 'returns the json data for the errors' do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response).to have_key(:errors)
+      end
+
+    end
+    
+  end
+  
+
 end
