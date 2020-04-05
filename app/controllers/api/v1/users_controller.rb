3b45d4e5d281b36400f75810c732d5bcac542a19
Nixon da Silva Palmeira <nixonnsp@gmail.com>
2020-04-05 15:48:25 +0000

    implementado action update e destroy com testes

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -28,7 +28,12 @@ class Api::V1::UsersController < ApplicationController
     else
       render json: { errors: user.errors}, status: 422
     end
+  end
 
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    head 204
   end
 
   private
--- spec/controllers/requests/api/v1/users_spec.rb
+++ spec/controllers/requests/api/v1/users_spec.rb
@@ -100,5 +100,18 @@ RSpec.describe 'Users API', type: :request do
     
   end
   
+  describe 'DELETE /users/:id' do
+    before do
+      headers = {'Accept' => 'application/vnd.taskmanager.v1'}
+      delete "/users/#{user_id}", params: {}, headers: headers
+    end
 
+    it 'return status code 204' do
+      expect(response).to have_http_status(204)  
+    end
+
+    it 'removes the user from dabase' do
+      expect(User.find_by(id: user_id)).to be_nil
+    end
+  end
 end
