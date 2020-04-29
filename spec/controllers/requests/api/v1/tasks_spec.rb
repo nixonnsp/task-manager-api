8ff62a309c212e61d8b6ea8aeb37ea76fa1ea82c
Nixon da Silva Palmeira <nixonnsp@gmail.com>
2020-04-29 22:49:04 +0000

    Adicionando action show no controler com testes

--- spec/controllers/requests/api/v1/tasks_spec.rb
+++ spec/controllers/requests/api/v1/tasks_spec.rb
@@ -28,4 +28,19 @@ RSpec.describe 'Tasks API', type: :request do
       expect(json_body[:tasks].count).to eq(5)
     end
   end
+
+  describe 'GET /tasks/:id' do
+    let(:task) { create(:task, user_id: user.id) }
+
+    before { get "/tasks/#{task.id}", params: {}, headers: headers }
+    
+    it 'returns status code 200' do
+      expect(response).to have_http_status(200) 
+    end
+
+    it 'return the json for task' do
+      expect(json_body[:title]).to eq(task.title)  
+    end    
+  end
+  
 end
