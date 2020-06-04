e28cf1ffe63472effe68b14dd0c9210790c07d58
Nixon da Silva Palmeira <nixonnsp@gmail.com>
2020-06-04 13:30:36 +0000

    autalizando a user api

--- spec/controllers/requests/api/v2/users_spec.rb
+++ spec/controllers/requests/api/v2/users_spec.rb
@@ -2,25 +2,29 @@ require 'rails_helper'
 
 RSpec.describe 'Users API', type: :request do
   let!(:user) { create(:user) }
-  let(:user_id) {user.id}
+  #let!(:user_id) {user.id}
+  let!(:auth_data) { user.create_new_auth_token }
   let(:headers) do
     {
       'Accept' => 'application/vnd.taskmanager.v2',
       'Content-Type' => Mime[:json].to_s,
-      'Authorization' => user.auth_token
+      'access-token' => auth_data['access-token'],
+      'uid' => auth_data['uid'],
+      'client' => auth_data['client']
     }
   end
 
   before { host! 'api.task-manager.text'} 
 
-  describe 'GET /users/:id' do
-    before do
-      get "/users/#{user_id}", params: {}, headers: headers
-    end
+  describe 'GET /auth/validate_token' do
     
-    context 'when the user exists' do
+    context 'when the request headers are valid' do
+      before do
+        get '/auth/validate_token', params: {}, headers: headers
+      end
+      
       it 'returns the user' do               
-        expect(json_body[:data][:id].to_i).to eq(user_id)  
+        expect(json_body[:data][:id].to_i).to eq(user.id)  
       end
 
       it 'returns status code 200' do
@@ -28,29 +32,33 @@ RSpec.describe 'Users API', type: :request do
       end
     end 
     
-    context 'when the user does not exist' do
-      let(:user_id) {1000}
-      it 'returns status code 404' do
-        expect(response).to have_http_status(404)
+    context 'when the request headers are invalid' do
+      before do
+        headers['access-token'] = "invalid_token"
+        get '/auth/validate_token', params: {}, headers: headers
+      end
+
+      it 'returns status code 401' do
+        expect(response).to have_http_status(401)
       end
     end
   end
 
-  describe 'POST /users' do
+  describe 'POST /auth' do
     before do
-      post '/users', params: { user: user_params}.to_json, headers: headers
+      post '/auth', params: user_params.to_json, headers: headers
     end
 
     context 'when the request params are valid' do
       let(:user_params) { attributes_for(:user) }
 
-      it 'return status code 201' do
-        expect(response).to have_http_status(201)        
+      it 'return status code 200' do
+        expect(response).to have_http_status(200)        
       end
 
       it 'returns json data for the created user' do
         
-        expect(json_body[:data][:attributes][:email]).to eq(user_params[:email])
+        expect(json_body[:data][:email]).to eq(user_params[:email])
       end
     end
 
@@ -68,9 +76,9 @@ RSpec.describe 'Users API', type: :request do
     end
   end
   
-  describe 'PUT /users/:id' do
+  describe 'PUT /auth' do
     before do
-      put "/users/#{user_id}", params: { user: user_params}.to_json, headers: headers
+      put '/auth', params: user_params.to_json, headers: headers
     end
 
     context 'when the request params are valid' do
@@ -81,7 +89,7 @@ RSpec.describe 'Users API', type: :request do
       end
 
       it 'returns the json data for the updated user' do        
-        expect(json_body[:data][:attributes][:email]).to eq(user_params[:email]) 
+        expect(json_body[:data][:email]).to eq(user_params[:email]) 
       end
 
     end
@@ -102,17 +110,17 @@ RSpec.describe 'Users API', type: :request do
     
   end
   
-  describe 'DELETE /users/:id' do
+  describe 'DELETE /auth' do
     before do      
-      delete "/users/#{user_id}", params: {}, headers: headers
+      delete '/auth', params: {}, headers: headers
     end
 
-    it 'return status code 204' do
-      expect(response).to have_http_status(204)  
+    it 'return status code 200' do
+      expect(response).to have_http_status(200)  
     end
 
     it 'removes the user from dabase' do
-      expect(User.find_by(id: user_id)).to be_nil
+      expect(User.find_by(id: user.id)).to be_nil
     end
   end
 end
